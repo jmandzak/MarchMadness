6157d47b486e3c0455f3cd291cc979dc57ba95a4
Josh Mandzak <jmandzak99@gmail.com>
2024-03-19 21:29:44 -0400

    Add 2024 matchup extraction

--- data/extract_from_tournament_results.py
+++ data/extract_from_tournament_results.py
@@ -36,11 +36,22 @@ def write_all_matchup_differentials(df: pd.DataFrame) -> None:
     new_df.to_csv('all_matchup_differentials.csv', index=False)
 
 
+def write_2024_matchups(df: pd.DataFrame) -> None:
+    # Get the rows that have 2024 as the value of the YEAR column
+    df = df[df['YEAR'] == 2024]
+
+    # For every two rows in the dataframe, get the TEAM column value and the SCORE column value and figure out the winner
+    new_df = pd.DataFrame()
+    new_df['TEAM1'] = df['TEAM'].iloc[::2].reset_index(drop=True)
+    new_df['TEAM2'] = df['TEAM'].iloc[1::2].reset_index(drop=True)
+    new_df.to_csv('2024_matchups.csv', index=False)
 
 
 def main():
     df = pd.read_csv('tournament_matchups.csv')
     df = clean_name_column(df)
+
+    write_2024_matchups(df)
 
     write_team_seeds_by_year(df)
 
